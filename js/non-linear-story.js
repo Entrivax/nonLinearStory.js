84ed1bdff211c6ccfaa230ebbd0d55b127779ae9
Entrivax <entrivax@gmail.com>
2018-04-29 03:40:13 +0200

    Initial commit

---
+++ js/non-linear-story.js
@@ -0,0 +1,119 @@
+;(function($) {
+
+    function NonLinearStory(element, history, initialStep) {
+        var _self = this;
+        _self.element = $(element);
+        _self.history = history;
+
+        _self.displayStep = function(step) {
+            _self.element.fadeOut(200, function() {
+                if (typeof step.onDisplay === 'function')
+                    step.onDisplay(_self.history);
+                var elem = _self.element.empty();
+                for (var i = 0; i < step.elements.length; i++) {
+                    (function (i) {
+                        var e = step.elements[i];
+                        if (typeof e === 'function')
+                            e = e(_self.history);
+                        if (typeof e === 'object' && e instanceof NLSAction) {
+                            $('<div class="action">' + step.elements[i].displayedName + '</div>')
+                                .bind('click', function() {
+                                    if (typeof step.elements[i].onClick === 'function')
+                                        step.elements[i].onClick(_self.history);
+
+                                    var stepToGo = '';
+
+                                    if (typeof step.elements[i].goToStep === 'string') {
+                                        stepToGo = step.elements[i].goToStep;
+                                    } else if (typeof step.elements[i].goToStep === 'function') {
+                                        stepToGo = step.elements[i].goToStep(history);
+                                    }
+
+                                    var stepObject = _self.getStep(stepToGo);
+
+                                    if (stepObject !== undefined) {
+                                        _self.displayStep(stepObject);
+                                    } else {
+                                        console.error('Step ' + stepToGo + ' not found!');
+                                    }
+                                }).appendTo(elem);
+                        } else {
+                            var elemToAppend = $('<div class="element"></div>');
+                            elemToAppend.html(e);
+                            elemToAppend.appendTo(elem);
+                        }
+                    })(i);
+                }
+                _self.element.fadeIn(200);
+            })
+        };
+
+        _self.getStep = function(stepName) {
+            for (var j = 0; j < history.steps.length; j++) {
+                if (history.steps[j].name === stepName) {
+                    return history.steps[j];
+                }
+            }
+        };
+
+        _self.init(initialStep);
+    }
+
+    NonLinearStory.prototype.init = function(initialStep) {
+        if (typeof initialStep === 'string') {
+            this.displayStep(this.getStep(initialStep));
+        } else {
+            this.displayStep(initialStep);
+        }
+    };
+
+    $.fn.nonLinearStory = function(history, initialStep) {
+        return this.each(function() {
+            if (!$.data(this, 'plugin_nonLinearStory')) {
+                $.data(this, 'plugin_nonLinearStory', 
+                new NonLinearStory(this, history, initialStep));
+            }
+        });
+    }
+})(jQuery);
+
+/**
+ * Describe an action of an NLHStep
+ * @constructor NLHAction
+ * @param {string} displayedName Name put in link
+ * @param {string|function(NLSStory)} goToStep Name of the step to go, or a function returning the name of the step to go as string
+ * @param {function(NLSStory)} onClick Function executed when option is clicked
+ */
+function NLSAction(displayedName, goToStep, onClick) {
+    this.displayedName = displayedName;
+    this.goToStep = goToStep;
+    this.onClick = onClick;
+
+    return this;
+}
+
+/**
+ * Describe a step of an NLSStory
+ * @constructor NLHStep
+ * @param {string} name The name of the step
+ * @param {string[]|function(NLSStory)[]|NLSAction[]} elements The html representing the step, or actions, or a function returning the html to display or an action
+ * @param {function(NLSStory)} onDisplay Function to execute when step is displayed
+ */
+function NLSStep(name, elements, onDisplay) {
+    this.name = name;
+    this.elements = elements;
+    this.onDisplay = onDisplay;
+
+    return this;
+}
+
+/**
+ * Describe an story
+ * @constructor NLHStory
+ * @param {NLSStep[]} steps List of steps composing an story
+ */
+function NLSStory(steps) {
+    this.steps = steps;
+
+    return this;
+}
---
+++ js/story.js
@@ -0,0 +1,30 @@
+;(function($) {
+    $(document).ready(function() {
+        var story = new NLSStory([
+            new NLSStep('startStep', [
+                'Bienvenue dans ce jeu !',
+                new NLSAction('Cliquez ici pour commencer le jeu.', 'step1', function(story) {
+                    story['jeuCommencé'] += 1;
+                }),
+            ]),
+            new NLSStep('step1', [
+                'Bien, vous savez suivre des instructions, c\'est déjà pas trop mal !',
+                new NLSAction('Dire coucou.', 'step2'),
+                new NLSAction('Recommencer.', 'startStep'),
+            ]),
+            new NLSStep('step2', [
+                'Oh, et bien coucou.',
+                new NLSAction('Demander comment ça va.', 'step3'),
+            ]),
+            new NLSStep('step3', [
+                'Mmmh, ça va.',
+                new NLSAction('Terminer le jeu.', 'startStep', function(story) {
+                    story['jeuTerminé'] += 1;
+                }),
+            ]),
+        ]);
+        story['jeuTerminé'] = 0;
+        story['jeuCommencé'] = 0;
+        $('nls').nonLinearStory(story, 'startStep');
+    });
+})(jQuery);
